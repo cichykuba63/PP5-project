b3b9a1a79fac7b0d4414fc5d7f089521a6a3c61e
cichykuba63 <jakub.slowiak9@o2.pl>
2024-11-21 12:36:35 +0100

    add footer, animations (aos, animista), add _index.scss, _animations.scss and _components.scss to enhance the clarity of the code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
 	const nav = document.querySelector('.navbar')
 	const navBtn = document.querySelector('.navbar-toggler')
-    const navList = document.querySelector('.navbar-collapse')
-    const allNavItems = document.querySelectorAll('.nav-link')
+	const navList = document.querySelector('.navbar-collapse')
+	const allNavItems = document.querySelectorAll('.nav-link')
+	const yearElement = document.querySelector('.year')
+	const currentYear = new Date().getFullYear()
+	const headerBtn = document.querySelector('header .btn')
 
 	function addShadow() {
 		if (window.scrollY >= 100) {
@@ -16,16 +19,29 @@ document.addEventListener('DOMContentLoaded', function () {
 		if (window.scrollY < 100) {
 			nav.classList.toggle('shadow-bg')
 		}
-    }
+	}
 
-    allNavItems.forEach(link => link.addEventListener('click', () => {
-        navList.classList.remove('show')
+	function headerBtnHover() {
+		headerBtn.classList.add('header-btn-hover')
+	}
 
-        if (window.scrollY < 100) {
-            nav.classList.remove('shadow-bg')
-        }
-    }))
+	function closeNavIfClickOutside(event) {
+		if (!nav.contains(event.target) && !navBtn.contains(event.target)) {
+			navList.classList.remove('show')
+			btnClick()
+		}
+	}
 
+	allNavItems.forEach(link =>
+		link.addEventListener('click', () => {
+			navList.classList.remove('show')
+			btnClick()
+		})
+	)
+
+	document.addEventListener('click', closeNavIfClickOutside)
 	window.addEventListener('scroll', addShadow)
 	navBtn.addEventListener('click', btnClick)
+	headerBtn.addEventListener('mouseover', headerBtnHover)
+	yearElement.textContent = `${currentYear}`
 })
